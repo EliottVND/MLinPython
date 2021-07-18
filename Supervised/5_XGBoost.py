3829a09373fc73092a67c104e9b03530d414bd05
Eliott Van Dieren <vandieren.eliott@gmail.com>
2021-07-18 22:26:01 +0200

    Added chapter 2

--- Supervised/5_XGBoost.py
+++ Supervised/5_XGBoost.py
@@ -91,3 +91,109 @@ print((cv_results["test-auc-mean"]).iloc[-1])
 #    3        0.822959       0.002018       0.821682      0.003912
 #    4        0.827528       0.000769       0.826191      0.001937
 #    0.826191 -> Gives us the AUC score :)
+
+
+# Comparaison tree base learners vs linear base learners
+
+
+# Create the training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+
+# Instantiate the XGBRegressor: xg_reg
+xg_reg = xgb.XGBRegressor(seed=123,objective='reg:linear',n_estimators=10)
+
+# Fit the regressor to the training set
+xg_reg.fit(X_train,y_train)
+
+# Predict the labels of the test set: preds
+preds = xg_reg.predict(X_test)
+
+# Compute the rmse: rmse
+rmse = np.sqrt(mean_squared_error(y_test, preds))
+print("RMSE: %f" % (rmse)) # 78847.401758
+
+
+
+
+# Convert the training and testing sets into DMatrixes: DM_train, DM_test
+DM_train = xgb.DMatrix(data=X_train,label=y_train)
+DM_test =  xgb.DMatrix(data=X_test,label=y_test)
+
+# Create the parameter dictionary: params
+params = {"booster":"gblinear", "objective":"reg:linear"}
+
+# Train the model: xg_reg
+xg_reg = xgb.train(dtrain = DM_train, num_boost_round=5, params=params)
+
+# Predict the labels of the test set: preds
+preds = xg_reg.predict(DM_test)
+
+# Compute and print the RMSE
+rmse = np.sqrt(mean_squared_error(y_test,preds))
+print("RMSE: %f" % (rmse)) # 43803.786107 -> Worked better
+
+
+#Cross Validation between RMSE and MAE objectives:
+
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics="rmse", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Extract and print final boosting round metric
+print((cv_results["test-rmse-mean"]).tail(1)) # 51348.28125
+
+
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics='mae', as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Extract and print final boosting round metric
+print((cv_results["test-mae-mean"]).tail(1)) # 35670.646485
+
+
+#Some kind of hyperparameter tuning on l2 regularization
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+reg_params = [1, 10, 100]
+
+# Create the initial parameter dictionary for varying l2 strength: params
+params = {"objective":"reg:linear","max_depth":3}
+
+# Create an empty list for storing rmses as a function of l2 complexity
+rmses_l2 = []
+
+# Iterate over reg_params
+for reg in reg_params:
+
+    # Update l2 strength
+    params["lambda"] = reg
+    
+    # Pass this updated param dictionary into cv
+    cv_results_rmse = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=2, num_boost_round=5, metrics="rmse", as_pandas=True, seed=123)
+    
+    # Append best rmse (final round) to rmses_l2
+    rmses_l2.append(cv_results_rmse["test-rmse-mean"].tail(1).values[0])
+
+# Look at best rmse per l2 param
+print("Best rmse as a function of l2:")
+print(pd.DataFrame(list(zip(reg_params, rmses_l2)), columns=["l2", "rmse"]))

4b13753d9063f613af26e156fc8c7d9ba7588697
Eliott Van Dieren <vandieren.eliott@gmail.com>
2021-07-30 00:15:40 +0200

    test sublime merge

---
+++ Supervised/7_dimension_reduction.py
@@ -0,0 +1,155 @@
+# Leave this list as is
+number_cols = ['HP', 'Attack', 'Defense']
+
+# Remove the feature without variance from this list
+non_number_cols = ['Name', 'Type']
+
+# Create a new dataframe by subselecting the chosen features
+df_selected = pokemon_df[number_cols + non_number_cols]
+
+# Prints the first 5 lines of the new dataframe
+print(df_selected.head())
+
+
+# Feature drop vs feature extraction
+
+#We remove correlated features 
+
+# Create a pairplot and color the points using the 'Gender' feature
+sns.pairplot(ansur_df_1, hue='Gender', diag_kind='hist')
+
+# Show the plot
+plt.show()
+
+# Remove one of the redundant features
+reduced_df = ansur_df_1.drop('stature_m', axis=1)
+
+# Create a pairplot and color the points using the 'Gender' feature
+sns.pairplot(reduced_df, hue='Gender')
+
+# Show the plot
+plt.show()
+
+# Create a pairplot and color the points using the 'Gender' feature
+sns.pairplot(ansur_df_2, hue='Gender', diag_kind='hist')
+
+
+# Show the plot
+plt.show()
+
+# We remove constants
+
+# Remove the redundant feature
+reduced_df = ansur_df_2.drop('n_legs', axis=1)
+
+# Create a pairplot and color the points using the 'Gender' feature
+sns.pairplot(reduced_df, hue='Gender', diag_kind='hist')
+
+# Show the plot
+plt.show()
+
+
+# Use t-SNE
+
+# Non-numerical columns in the dataset
+non_numeric = ['Branch', 'Gender', 'Component']
+
+# Drop the non-numerical columns from df
+df_numeric = df.drop(non_numeric, axis=1)
+
+# Create a t-SNE model with learning rate 50
+m = TSNE(learning_rate=50)
+
+# Fit and transform the t-SNE model on the numeric dataset
+tsne_features = m.fit_transform(df_numeric)
+print(tsne_features.shape) # Reduced dataset from 90 to 2 features which we can now plot
+
+# Color the points by Gender
+sns.scatterplot(x="x", y="y", hue='Gender', data=df)
+
+# Show the plot
+plt.show() # we see a big differnce :)
+
+
+# Lets talk about overfitting
+
+# Import train_test_split()
+from sklearn.model_selection import train_test_split
+
+# Select the Gender column as the feature to be predicted (y)
+y = ansur_df['Gender']
+
+# Remove the Gender column to create the training data
+X = ansur_df.drop('Gender', axis=1)
+
+# Perform a 70% train and 30% test data split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+print("{} rows in test set vs. {} in training set. {} Features.".format(X_test.shape[0], X_train.shape[0], X_test.shape[1]))
+
+
+# Import SVC from sklearn.svm and accuracy_score from sklearn.metrics
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# Create an instance of the Support Vector Classification class
+svc = SVC()
+
+# Fit the model to the training data
+svc.fit(X_train, y_train)
+
+# Calculate accuracy scores on both train and test data
+accuracy_train = accuracy_score(y_train, svc.predict(X_train))
+accuracy_test = accuracy_score(y_test, svc.predict(X_test))
+
+print("{0:.1%} accuracy on test set vs. {1:.1%} on training set".format(accuracy_test, accuracy_train)) # Prints out 48% for testing et 100% for training --> overfitting
+
+# Exemple ou ça n'overfit pas
+
+
+# Assign just the 'neckcircumferencebase' column from ansur_df to X
+X = ansur_df[['neckcircumferencebase']]
+
+# Split the data, instantiate a classifier and fit the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+svc = SVC()
+svc.fit(X_train, y_train)
+
+# Calculate accuracy scores on both train and test data
+accuracy_train = accuracy_score(y_train, svc.predict(X_train))
+accuracy_test = accuracy_score(y_test, svc.predict(X_test))
+
+print("{0:.1%} accuracy on test set vs. {1:.1%} on training set".format(accuracy_test, accuracy_train))
+
+
+
+# Using masks to reduce dimension
+
+from sklearn.feature_selection import VarianceThreshold
+
+# Create a VarianceThreshold feature selector
+sel = VarianceThreshold(threshold=0.001)
+
+# Fit the selector to normalized head_df
+sel.fit(head_df / head_df.mean())
+
+# Create a boolean mask
+mask = sel.get_support()
+
+# Apply the mask to create a reduced dataframe
+reduced_df = head_df.loc[:, mask]
+
+print("Dimensionality reduced from {} to {}.".format(head_df.shape[1], reduced_df.shape[1]))
+
+
+# Applying masks on standardized data
+
+
+# Create a boolean mask on whether each feature less than 50% missing values.
+mask = school_df.isna().sum() / len(school_df) < 0.5
+
+# Create a reduced dataset by applying the mask
+reduced_df = school_df.loc[:,mask]
+
+print(school_df.shape)
+print(reduced_df.shape)

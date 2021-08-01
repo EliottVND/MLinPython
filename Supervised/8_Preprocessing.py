1fbc8c7dd8c4c9a35b06cf66713cc20241f8e0d7
Eliott Van Dieren <vandieren.eliott@gmail.com>
2021-08-02 00:33:41 +0200

    Added first chapter on PreProcessing

---
+++ Supervised/8_Preprocessing.py
@@ -0,0 +1,22 @@
+# Check how many values are missing in the category_desc column
+print(volunteer['category_desc'].isnull().sum())
+
+# Subset the volunteer dataset
+volunteer_subset = volunteer[volunteer['category_desc'].notnull()] #only keeps rows where volunteer['category_desc'] is not null
+
+# Print out the shape of the subset
+print(volunteer_subset.shape)
+
+# Using value counts and train_test_split
+
+# Create a data with all columns except category_desc
+volunteer_X = volunteer.drop('category_desc', axis=1)
+
+# Create a category_desc labels dataset
+volunteer_y = volunteer[['category_desc']]
+
+# Use stratified sampling to split up the dataset according to the volunteer_y dataset
+X_train, X_test, y_train, y_test = train_test_split(volunteer_X, volunteer_y, stratify=volunteer_y)
+
+# Print out the category_desc counts on the training y labels
+print(y_train['category_desc'].value_counts())

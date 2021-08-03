57148a898fff1b438154a0da6eec0fa43f626d21
Eliott Van Dieren <vandieren.eliott@gmail.com>
2021-08-04 00:27:32 +0200

    Added Chapter 2 and 3 of preprocessing

--- Supervised/8_Preprocessing.py
+++ Supervised/8_Preprocessing.py
@@ -20,3 +20,149 @@ X_train, X_test, y_train, y_test = train_test_split(volunteer_X, volunteer_y, st
 
 # Print out the category_desc counts on the training y labels
 print(y_train['category_desc'].value_counts())
+
+
+## Standardization ##
+
+# Split the dataset and labels into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Fit the k-nearest neighbors model to the training data
+knn.fit(X_train,y_train)
+
+# Score the model on the test data
+print(knn.score(X_test,y_test))
+
+
+# Using Log Normalization
+
+# Print out the variance of the Proline column
+print(wine['Proline'].var())
+
+# Apply the log normalization function to the Proline column
+wine['Proline_log'] = np.log(wine['Proline'])
+
+# Check the variance of the normalized Proline column
+print(wine['Proline_log'].var())
+
+# Using StandardScaler
+
+# Import StandardScaler from scikit-learn
+from sklearn.preprocessing import StandardScaler
+
+# Create the scaler
+ss = StandardScaler()
+
+# Take a subset of the DataFrame you want to scale 
+wine_subset = wine[['Ash','Alcalinity of ash','Magnesium']]
+
+# Apply the scaler to the DataFrame subset
+wine_subset_scaled = ss.fit_transform(wine_subset)
+
+# A bit of knn just for fun
+
+# Split the dataset and labels into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X,y)
+
+# Fit the k-nearest neighbors model to the training data
+knn.fit(X_train, y_train)
+
+# Score the model on the test data
+print(knn.score(X_test,y_test))
+
+
+# Create the scaling method.
+ss = StandardScaler()
+
+# Apply the scaling method to the dataset used for modeling.
+X_scaled = ss.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y)
+
+# Fit the k-nearest neighbors model to the training data.
+knn.fit(X_train,y_train)
+
+# Score the model on the test data.
+print(knn.score(X_test,y_test)) # Gives us a better accuracy.
+
+
+# Label Encoder : Seems to work only with 2 variables (pour faire un binary 0 ou 1)
+
+# Set up the LabelEncoder object
+enc = LabelEncoder()
+
+# Apply the encoding to the "Accessible" column
+hiking['Accessible_enc'] = enc.fit_transform(hiking['Accessible'])
+
+# Compare the two columns
+print(hiking[['Accessible_enc', 'Accessible']].head())
+
+# If more than 2 variables, use get dummies / One Hot Encoder
+
+# Transform the category_desc column
+category_enc = pd.get_dummies(volunteer["category_desc"])
+
+# Take a look at the encoded columns
+print(category_enc.head())
+
+# Feature Engineerign : Creating mean ccolums from different features
+
+# Create a list of the columns to average
+run_columns = ['run1','run2','run3','run4','run5']
+
+# Use apply to create a mean column
+running_times_5k["mean"] = running_times_5k.apply(lambda row: row[run_columns].mean(), axis=1)
+
+# Take a look at the results
+print(running_times_5k)
+
+# Extracting months instead of entire dates
+
+# First, convert string column to date column
+volunteer["start_date_converted"] = pd.to_datetime(volunteer['start_date_date'])
+
+# Extract just the month from the converted column
+volunteer["start_date_month"] = volunteer["start_date_converted"].apply(lambda row: row.month)
+
+# Take a look at the converted and new month columns
+print(volunteer[['start_date_converted', 'start_date_month']].head())
+
+# Engineering features from strings - extraction
+
+# Write a pattern to extract numbers and decimals
+def return_mileage(length):
+    pattern = re.compile(r"\d+\.\d+")
+    
+    # Search the text for matches
+    mile = re.match(pattern, length)
+    
+    # If a value is returned, use group(0) to return the found value
+    if mile is not None:
+        return float(mile.group(0))
+        
+# Apply the function to the Length column and take a look at both columns
+hiking["Length_num"] = hiking['Length'].apply(lambda row: return_mileage(row))
+print(hiking[["Length", "Length_num"]].head())
+
+# Engineering features from strings - tf/idf
+
+# Take the title text
+title_text = volunteer['title']
+
+# Create the vectorizer method
+tfidf_vec = TfidfVectorizer()
+
+# Transform the text into tf-idf vectors
+text_tfidf = tfidf_vec.fit_transform(title_text) 
+
+
+# Using tfidf_vec to classify text
+
+# Split the dataset according to the class distribution of category_desc
+y = volunteer["category_desc"]
+X_train, X_test, y_train, y_test = train_test_split(text_tfidf.toarray(), y, stratify=y)
+
+# Fit the model to the training data
+nb.fit(X_train, y_train)
+
+# Print out the model's accuracy 
+print(nb.score(X_test, y_test)) # O.56

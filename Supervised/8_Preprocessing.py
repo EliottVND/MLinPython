881bfd1de91766f8faf5d93844b5b9fbf0c17a2c
Eliott Van Dieren <vandieren.eliott@gmail.com>
2021-08-04 17:48:15 +0200

    Finished Preprocessing + 1rst chapter time series

--- Supervised/8_Preprocessing.py
+++ Supervised/8_Preprocessing.py
@@ -166,3 +166,195 @@ nb.fit(X_train, y_train)
 
 # Print out the model's accuracy 
 print(nb.score(X_test, y_test)) # O.56
+
+# Selecting non-redundant features 
+
+
+# Create a list of redundant column names to drop
+to_drop = ["locality", "region", "created_date", "vol_requests", "category_desc"]
+
+# Drop those columns from the dataset
+volunteer_subset = volunteer.drop(to_drop, axis=1)
+
+# Print out the head of the new dataset
+print(volunteer_subset.head())
+
+# Using correlation to see if a feature is useful or not
+
+# Print out the column correlations of the wine dataset
+print(wine.corr())
+
+# Take a minute to find the column where the correlation value is greater than 0.75 at least twice
+to_drop = "Flavanoids"
+
+# Drop that column from the DataFrame
+wine = wine.drop(to_drop, axis=1)
+
+# Using text vectors
+
+# Add in the rest of the parameters
+def return_weights(vocab, original_vocab, vector, vector_index, top_n):
+    zipped = dict(zip(vector[vector_index].indices, vector[vector_index].data))
+    
+    # Let's transform that zipped dict into a series
+    zipped_series = pd.Series({vocab[i]:zipped[i] for i in vector[vector_index].indices})
+    
+    # Let's sort the series to pull out the top n weighted words
+    zipped_index = zipped_series.sort_values(ascending=False)[:top_n].index
+    return [original_vocab[i] for i in zipped_index]
+
+# Print out the weighted words
+print(return_weights(vocab, tfidf_vec.vocabulary_, text_tfidf,8, 3))
+
+
+def words_to_filter(vocab, original_vocab, vector, top_n):
+    filter_list = []
+    for i in range(0, vector.shape[0]):
+    
+        # Here we'll call the function from the previous exercise, and extend the list we're creating
+        filtered = return_weights(vocab, original_vocab, vector, i, top_n)
+        filter_list.extend(filtered)
+    # Return the list in a set, so we don't get duplicate word indices
+    return set(filter_list)
+
+# Call the function to get the list of word indices
+filtered_words = words_to_filter(vocab, tfidf_vec.vocabulary_, text_tfidf, 3)
+
+# By converting filtered_words back to a list, we can use it to filter the columns in the text vector
+filtered_text = text_tfidf[:, list(filtered_words)]
+
+
+### Récap :) ###
+
+
+
+# Check the column types
+print(ufo.dtypes)
+
+# Change the type of seconds to float
+ufo["seconds"] = ufo["seconds"].astype(float)
+
+# Change the date column to type datetime
+ufo["date"] = pd.to_datetime(ufo["date"])
+
+# Check the column types
+print(ufo[['seconds','date']].dtypes)
+
+
+# Check how many values are missing in the length_of_time, state, and type columns
+print(ufo[['length_of_time', 'state', 'type']].isnull().sum())
+
+# Keep only rows where length_of_time, state, and type are not null
+ufo_no_missing = ufo[ufo['length_of_time'].notnull() & 
+          ufo['state'].notnull() & 
+          ufo['type'].notnull()]
+
+# Print out the shape of the new dataset
+print(ufo_no_missing.shape)
+
+# Extracting number from string
+
+def return_minutes(time_string):
+
+    # Use \d+ to grab digits
+    pattern = re.compile(r"\d+")
+    
+    # Use match on the pattern and column
+    num = re.match(pattern, time_string)
+    if num is not None:
+        return int(num.group(0))
+        
+# Apply the extraction to the length_of_time column
+ufo["minutes"] = ufo["length_of_time"].apply(lambda row : return_minutes(row))
+
+# Take a look at the head of both of the columns
+print(ufo[['minutes','length_of_time']].head())
+
+# Log Normalization
+
+# Check the variance of the seconds and minutes columns
+print(ufo[['seconds','minutes']].var()) # 40k et 128    
+
+# Log normalize the seconds column
+ufo["seconds_log"] = np.log(ufo['seconds'])
+
+# Print out the variance of just the seconds_log column
+print(ufo["seconds_log"].var()) # 1.qqch
+
+
+
+# Use Pandas to encode us values as 1 and others as 0
+ufo["country_enc"] = ufo["country"].apply(lambda x: 1 if x == 'us' else 0)
+
+# Print the number of unique type values
+print(len(ufo["type"].unique()))
+
+# Create a one-hot encoded set of the type values
+type_set = pd.get_dummies(ufo["type"])
+
+# Concatenate this set back to the ufo DataFrame
+ufo = pd.concat([ufo, type_set], axis=1)    
+
+
+
+# Look at the first 5 rows of the date column
+print(ufo['date'].head(5))
+
+# Extract the month from the date column
+ufo["month"] = ufo["date"].apply(lambda x : x.month)
+
+# Extract the year from the date column
+ufo["year"] = ufo["date"].apply(lambda x : x.year)
+
+# Take a look at the head of all three columns
+print(ufo[['date','month','year']].head(5)) 
+
+
+
+# Take a look at the head of the desc field
+print(ufo['desc'].head())
+
+# Create the tfidf vectorizer object
+vec = TfidfVectorizer()
+
+# Use vec's fit_transform method on the desc field
+desc_tfidf = vec.fit_transform(ufo['desc'])
+
+# Look at the number of columns this creates
+print(desc_tfidf.shape)
+
+
+# Dropping features
+
+# Check the correlation between the seconds, seconds_log, and minutes columns
+print(ufo[['seconds','seconds_log','minutes']].corr())
+
+# Make a list of features to drop
+to_drop = ['city','country','date','desc','lat','length_of_time','long','minutes','recorded','seconds','state']
+
+# Drop those features
+ufo_dropped = ufo.drop(to_drop,axis=1)
+
+# Let's also filter some words out of the text vector we created
+filtered_words = words_to_filter(vocab, vec.vocabulary_, desc_tfidf, 4)
+
+
+
+
+# Take a look at the features in the X set of data
+print(X.columns)
+
+# Split the X and y sets using train_test_split, setting stratify=y
+train_X, test_X, train_y, test_y = train_test_split(X,y,stratify=y)
+
+# Fit knn to the training sets
+knn.fit(train_X,train_y)
+
+# Print the score of knn on the test sets
+print(knn.score(test_X,test_y))
+
+
+
+
+
+

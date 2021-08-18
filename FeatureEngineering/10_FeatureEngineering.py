d13adb2b53b0ccf3eebd1e8b324191d36e9bfe63
Eliott Van Dieren <vandieren.eliott@gmail.com>
2021-08-18 15:08:26 +0200

    Finished Feature Engineering Course

--- FeatureEngineering/10_FeatureEngineering.py
+++ FeatureEngineering/10_FeatureEngineering.py
@@ -170,3 +170,278 @@ print(so_survey_df['RawSalary'])
 # Create a boxplot of ConvertedSalary
 so_numeric_df[['ConvertedSalary']].boxplot()
 plt.show()
+
+
+
+
+
+# Scaling data
+
+# MinMaxScaler (Min = 0, Max = 1, le reste est compris entre 0 et 1)
+
+# Import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
+
+# Instantiate MinMaxScaler
+MM_scaler = MinMaxScaler()
+
+# Fit MM_scaler to the data
+MM_scaler.fit(so_numeric_df[['Age']])
+
+# Transform the data using the fitted scaler
+so_numeric_df['Age_MM'] = MM_scaler.transform(so_numeric_df[['Age']])
+
+# Compare the origional and transformed column
+print(so_numeric_df[['Age_MM', 'Age']].head())
+
+
+
+# Standard Scaler (= Linear Transformation) (Distribution autour de la moyenne avec des std comme valeurs)
+
+# Import StandardScaler
+from sklearn.preprocessing import StandardScaler
+
+# Instantiate StandardScaler
+SS_scaler = StandardScaler()
+
+# Fit SS_scaler to the data
+SS_scaler.fit(so_numeric_df[['Age']])
+
+# Transform the data using the fitted scaler
+so_numeric_df['Age_SS'] = SS_scaler.transform(so_numeric_df[['Age']])
+
+# Compare the origional and transformed column
+print(so_numeric_df[['Age_SS', 'Age']].head())
+
+
+
+
+# PowerTransformer (= Log Transformation)(Also centering around mean) Helps for skewed data
+
+# Import PowerTransformer
+from sklearn.preprocessing import PowerTransformer
+
+# Instantiate PowerTransformer
+pow_trans = PowerTransformer()
+
+# Train the transform on the data
+pow_trans.fit(so_numeric_df[['ConvertedSalary']])
+
+# Apply the power transform to the data
+so_numeric_df['ConvertedSalary_LG'] = pow_trans.transform(so_numeric_df[['ConvertedSalary']])
+
+# Plot the data before and after the transformation
+so_numeric_df[['ConvertedSalary', 'ConvertedSalary_LG']].hist()
+plt.show()
+
+
+
+# Deleting outliers (if wanted)
+
+# 1) deleting data > 95th percentile
+
+# Find the 95th quantile
+quantile = so_numeric_df['ConvertedSalary'].quantile(0.95)
+
+# Trim the outliers
+trimmed_df = so_numeric_df[so_numeric_df['ConvertedSalary'] < quantile]
+
+# The original histogram
+so_numeric_df[['ConvertedSalary']].hist()
+plt.show()
+plt.clf()
+
+# The trimmed histogram
+trimmed_df[['ConvertedSalary']].hist()
+plt.show()
+
+# 2) Deleting below 3 std and over 3 std
+
+# Find the mean and standard dev
+std = so_numeric_df['ConvertedSalary'].std()
+mean = so_numeric_df['ConvertedSalary'].mean()
+
+# Calculate the cutoff
+cut_off = std*3
+lower, upper = mean - cut_off, mean + cut_off
+
+# Trim the outliers
+trimmed_df = so_numeric_df[(so_numeric_df['ConvertedSalary'] < upper) 
+& (so_numeric_df['ConvertedSalary'] > lower)]
+
+# The trimmed box plot
+trimmed_df[['ConvertedSalary']].boxplot()
+plt.show()
+
+
+# When we use standardization, we train the transformer on the train set 
+# but we have to transform the train and test sets. We only transform the test set, not training
+# because we would use data leakage
+
+# Import StandardScaler
+from sklearn.preprocessing import StandardScaler
+
+# Apply a standard scaler to the data
+SS_scaler = StandardScaler()
+
+# Fit the standard scaler to the data
+SS_scaler.fit(so_train_numeric[['Age']])
+
+# Transform the test data using the fitted scaler
+so_test_numeric['Age_ss'] = SS_scaler.transform(so_test_numeric[['Age']])
+print(so_test_numeric[['Age', 'Age_ss']].head())
+
+
+
+
+train_std = so_train_numeric['ConvertedSalary'].std()
+train_mean = so_train_numeric['ConvertedSalary'].mean()
+
+cut_off = train_std * 3
+train_lower, train_upper = train_mean - cut_off, train_mean + cut_off
+
+# Trim the test DataFrame
+trimmed_df = so_test_numeric[(so_test_numeric['ConvertedSalary'] < train_upper) \
+                             & (so_test_numeric['ConvertedSalary'] > train_lower)]
+
+
+
+
+# TEXT manipulation
+
+# Print the first 5 rows of the text column
+print(speech_df['text'].head(5))
+
+# Replace all non letter characters with a whitespace
+speech_df['text_clean'] = speech_df['text'].str.replace('[^a-zA-Z]', ' ')
+
+# Change to lower case
+speech_df['text_clean'] = speech_df['text_clean'].str.lower()
+
+# Print the first 5 rows of the text_clean column
+print(speech_df['text_clean'].head())
+
+# Find the length of each text
+speech_df['char_cnt'] = speech_df['text_clean'].str.len()
+
+# Count the number of words in each text
+speech_df['word_cnt'] = speech_df['text_clean'].str.split().str.len()
+
+# Find the average length of word
+speech_df['avg_word_length'] = speech_df['char_cnt'] / speech_df['word_cnt']
+
+# Print the first 5 rows of these columns
+print(speech_df[['text_clean', 'char_cnt', 'word_cnt', 'avg_word_length']])
+
+# Using CountVectorizer
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Instantiate CountVectorizer
+cv = CountVectorizer()
+
+# Fit the vectorizer
+cv.fit(speech_df['text_clean'])
+
+# Print feature names
+print(cv.get_feature_names())
+
+# Apply the vectorizer
+cv_transformed = cv.transform(speech_df['text_clean'])
+
+# Print the full array
+cv_array = cv_transformed.toarray()
+
+# Print the shape of cv_array
+print(cv_array.shape) # (58, 9043) so 53 texts and 9043 unique words 
+
+# Using min_df and max_df to decrease the number of features
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Specify arguements to limit the number of features generated
+cv = CountVectorizer(min_df = 0.2, max_df = 0.8)
+
+# Fit, transform, and convert into array
+cv_transformed = cv.fit_transform(speech_df['text_clean'])
+cv_array = cv_transformed.toarray()
+
+# Print the array shape
+print(cv_array.shape) #(58, 818)
+
+# Concatenate the count results to initial df
+
+# Create a DataFrame with these features
+cv_df = pd.DataFrame(cv_array, 
+                     columns=cv.get_feature_names()).add_prefix('Counts_')
+
+# Add the new columns to the original DataFrame
+speech_df_new = pd.concat([speech_df, cv_df], axis=1, sort=False)
+print(speech_df_new.head())
+
+# Term frequency-inverse document frequency TF-idf
+
+
+# Import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Instantiate TfidfVectorizer
+tv = TfidfVectorizer(max_features=100,stop_words='english')
+
+# Fit the vectroizer and transform the data
+tv_transformed = tv.fit_transform(speech_df['text_clean'])
+
+# Create a DataFrame with these features
+tv_df = pd.DataFrame(tv_transformed.toarray(), 
+                     columns=tv.get_feature_names()).add_prefix('TFIDF_')
+print(tv_df.head())
+
+
+# Isolate the row to be examined
+sample_row = tv_df.iloc[0]
+
+# Print the top 5 words of the sorted output
+print(sample_row.sort_values(ascending=False).head())
+
+
+# Using train and test set
+
+# Instantiate TfidfVectorizer
+tv = TfidfVectorizer(max_features=100, stop_words='english')
+
+# Fit the vectroizer and transform the data
+tv_transformed = tv.fit_transform(train_speech_df['text_clean'])
+
+# Transform test data
+test_tv_transformed = tv.transform(test_speech_df['text_clean'])
+
+# Create new features for the test set
+test_tv_df = pd.DataFrame(test_tv_transformed.toarray(), 
+                          columns=tv.get_feature_names()).add_prefix('TFIDF_')
+print(test_tv_df.head())
+
+# Using NGram
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Instantiate a trigram vectorizer
+cv_trigram_vec = CountVectorizer(max_features=100, 
+                                 stop_words='english', 
+                                 ngram_range = (3,3))
+
+# Fit and apply trigram vectorizer
+cv_trigram = cv_trigram_vec.fit_transform(speech_df['text_clean'])
+
+# Print the trigram features
+print(cv_trigram_vec.get_feature_names())
+
+# Create a DataFrame of the features
+cv_tri_df = pd.DataFrame(cv_trigram.toarray(), 
+                 columns=cv_trigram_vec.get_feature_names()).add_prefix('Counts_')
+
+# Print the top 5 words in the sorted output
+print(cv_tri_df.sum().sort_values(ascending=False).head())
+

cd26af5f3eafbfce2b4733cf65ef636d70db1899
Eliott Van Dieren <vandieren.eliott@gmail.com>
2021-08-25 22:13:03 +0200

    finished course on Model Validation

--- ModelValidation/10_modelValidation.py
+++ ModelValidation/10_modelValidation.py
@@ -202,3 +202,174 @@ X_train, X_val, y_train, y_val =\
     # Print the train and test scores.
     print("The training scores were: {}".format(train_scores))
     print("The testing scores were: {}".format(test_scores))
+
+
+# Chapter 3
+
+# Showing how using only one test set is bad
+
+# Create two different samples of 200 observations 
+sample1 = tic_tac_toe.sample(200, random_state=1111)
+sample2 = tic_tac_toe.sample(200, random_state=1171)
+
+# Print the number of common observations 
+print(len([index for index in sample1.index if index in sample2.index]))
+
+# Print the number of observations in the Class column for both samples 
+print(sample1['Class'].value_counts())
+print(sample2['Class'].value_counts()) # differs
+
+# Leave One out cross validation
+# K-fold
+
+
+# KFold validation
+
+from sklearn.model_selection import KFold
+
+# Use KFold
+kf = KFold(n_splits=5, shuffle=True, random_state=1111)
+
+# Create splits
+splits = kf.split(X)
+
+# Print the number of indices
+for train_index, validation_index in splits:
+    print("Number of training indices: %s" % len(train_index))
+    print("Number of validation indices: %s" % len(validation_index))
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+rfc = RandomForestRegressor(n_estimators=25, random_state=1111)
+
+# Access the training and validation indices of splits
+for train_index, val_index in splits:
+    # Setup the training and validation data
+    X_train, y_train = X[train_index], y[train_index]
+    X_val, y_val = X[val_index], y[val_index]
+    # Fit the random forest model
+    rfc.fit(X_train, y_train)
+    # Make predictions, and print the accuracy
+    predictions = rfc.predict(X_val)
+    print("Split accuracy: " + str(mean_squared_error(y_val, predictions)))
+
+
+# CrossValidation using scikit-learn
+
+
+# Instruction 1: Load the cross-validation method
+from sklearn.model_selection import cross_val_score
+
+# Instruction 2: Load the random forest regression model
+from sklearn.ensemble import RandomForestRegressor
+
+# Instruction 3: Load the mean squared error method
+# Instruction 4: Load the function for creating a scorer
+from sklearn.metrics import mean_squared_error, make_scorer
+
+rfc = RandomForestRegressor(n_estimators=25, random_state=1111)
+mse = make_scorer(mean_squared_error)
+
+# Set up cross_val_score
+cv = cross_val_score(estimator=rfc,
+                     X=X_train,
+                     y=y_train,
+                     cv=10,
+                     scoring=mse)
+
+# Print the mean error
+print(cv.mean())
+
+
+# Leave one out the cross-validation
+# K-Fold with every datapoint as a fold and 1 left as validation set
+
+from sklearn.metrics import mean_absolute_error, make_scorer
+
+# Create scorer
+mae_scorer = make_scorer(mean_absolute_error)
+
+rfr = RandomForestRegressor(n_estimators=15, random_state=1111)
+
+# Implement LOOCV
+scores = cross_val_score(estimator=rfr, X=X, y=y, cv=X.shape[0], scoring=mae_scorer)
+
+# Print the mean and standard deviation
+print("The mean of the errors is: %s." % np.mean(scores))
+print("The standard deviation of the errors is: %s." % np.std(scores))
+
+# Parameter tuning (yes, again)
+
+# Review the parameters of rfr
+print(rfr.get_params())
+
+# Maximum Depth
+max_depth = [4, 8, 12]
+
+# Minimum samples for a split
+min_samples_split = [2, 5, 10]
+
+# Max features 
+max_features =[4, 6, 8, 10]
+
+# Using random.choice to select a random parameter
+
+
+from sklearn.ensemble import RandomForestRegressor
+
+# Fill in rfr using your variables
+rfr = RandomForestRegressor(
+    n_estimators=100,
+    max_depth=random.choice(max_depth),
+    min_samples_split=random.choice(min_samples_split),
+    max_features=random.choice(max_features))
+
+# Print out the parameters
+print(rfr.get_params())
+
+
+# Using RandomizedSearchCV
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import make_scorer, mean_squared_error
+
+# Finish the dictionary by adding the max_depth parameter
+param_dist = {"max_depth": [2,4,6,8],
+              "max_features": [2, 4, 6, 8, 10],
+              "min_samples_split": [2, 4, 8, 16]}
+
+# Create a random forest regression model
+rfr = RandomForestRegressor(n_estimators=10, random_state=1111)
+
+# Create a scorer to use (use the mean squared error)
+scorer = make_scorer(mean_squared_error)
+
+# Import the method for random RandomizedSearchCVrandom_search
+from sklearn.model_selection import RandomizedSearchCV
+
+# Build a random search using param_dist, rfr, and scorer
+random_search =\
+    RandomizedSearchCV(
+        estimator=rfr,
+        param_distributions=param_dist,
+        n_iter=10,
+        cv=5,
+        scoring=scorer)
+
+
+from sklearn.metrics import precision_score, make_scorer
+
+# Create a precision scorer
+precision = make_scorer(precision_score)
+# Finalize the random search
+rs = RandomizedSearchCV(
+  estimator=rfc, param_distributions=param_dist,
+  scoring = precision,
+  cv=5, n_iter=10, random_state=1111)
+rs.fit(X, y)
+
+# print the mean test scores:
+print('The accuracy for each run was: {}.'.format(rs.cv_results_['mean_test_score']))
+# print the best model score:
+print('The best accuracy for a single model was: {}'.format(rs.best_score_))
